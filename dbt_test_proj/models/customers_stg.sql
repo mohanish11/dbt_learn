c0cffd352ab168fc772911f8068527884324943b
mohanish11 <pithani_m@me.iitr.ac.in>
2025-10-24 12:33:55 +0530

    initial commit

---
+++ dbt_test_proj/models/customers_stg.sql
@@ -0,0 +1,16 @@
+{{ config(materialized='ephemeral') }}     
+
+SELECT
+    CustomerID,
+    FirstName,
+    LastName,
+    Email,
+    Phone,
+    Address,
+    City,
+    State,
+    ZipCode,
+    Updated_at,
+    CONCAT(FirstName, ' ', LastName) AS CustomerName
+FROM
+   L1_Landing.Customers
