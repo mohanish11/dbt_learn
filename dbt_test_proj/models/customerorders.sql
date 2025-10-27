ad5eb90a03ff6cf314705d2cba51c250dbf621d0
mohanish11 <pithani_m@me.iitr.ac.in>
2025-10-28 01:59:06 +0530

    new collumn addition

--- dbt_test_proj/models/customerorders.sql
+++ dbt_test_proj/models/customerorders.sql
@@ -1,6 +1,8 @@
 {{config(materialized='table')}}
 select c.customerid ,count(o.orderid) 
 as no_of_orders,
+count(o.orderid) 
+as no_of_orders2,
 concat(c.firstname,c.lastname) as customer_name from 
  sleekmart_oms.l1_landing.customers c join 
 sleekmart_oms.l1_landing.orders o on c.customerid=o.customerid
